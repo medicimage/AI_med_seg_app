fbc07fa15b4686819308d74664b81fb4f7ec78e6
medicimage <95139084+medicimage@users.noreply.github.com>
2021-11-27 18:39:33 +0000

    Update modelDeployment.py

--- modelDeployment.py
+++ modelDeployment.py
@@ -4,7 +4,7 @@ import funcs_ha_use
 import numpy as np
 from streamlit import caching
 
-@st.cache(suppress_st_warning=True)
+#@st.cache(suppress_st_warning=True)
 
 def runDeepSegmentationModel(organTarget, img):
 
